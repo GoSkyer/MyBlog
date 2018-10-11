3fe900df57aeb3ac6727716e9d956a55fc514b38
zoharSoul <102589617@qq.com>
2018-10-11 15:52:26 +0800

    :recycle: 发现注册中心改为使用eureka

--- config/src/main/java/org/gosky/blog/config/ConfigApplication.java
+++ config/src/main/java/org/gosky/blog/config/ConfigApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by guo on 2018/10/11.
@@ -13,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class ConfigApplication {
 
     public static void main(String[] args) {
