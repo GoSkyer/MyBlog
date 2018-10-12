7b58849d3d3d049157a64aab58bfe59e6b36e8ea
zoharSoul <102589617@qq.com>
2018-10-12 16:34:57 +0800

    :sparkles: 尝试添加base基础module

--- management/src/main/java/me/galaxy/blog/ManagementApplication.java
+++ management/src/main/java/me/galaxy/blog/ManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.galaxy.blog", "org.gosky.blog.base"})
 public class ManagementApplication {
 
     public static void main(String[] args) {
